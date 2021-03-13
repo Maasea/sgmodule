123699a154bacee8daa35270665a36303825b2b5
Maasea <41108003+Maasea@users.noreply.github.com>
2021-03-13 20:53:31 +0800

    Update enpass.js

--- script/enpass/enpass.js
+++ script/enpass/enpass.js
@@ -1,5 +1,3 @@
 var obj = JSON.parse($response.body);
-obj.license = "premium";
-obj.info.purchase_type = "premium";
-obj.info.store = "premium";
+obj.license = "pro";
 $done({ body: JSON.stringify(obj) });
