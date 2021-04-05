b94b048982faef7cb8093ada1bfcdb90c6d7ba40
maasea <351961611@qq.com>
2021-04-05 11:05:55 +0800

    update

--- script/keep/sections.js
+++ script/keep/sections.js
@@ -2,7 +2,7 @@ let obj = JSON.parse($response.body);
 let sections = obj.data.sections;
 for (i = 0; i < sections.length; i++) {
   let e = sections[i];
-  if (!(e.bizType == "ad" || e.bizType == "store_recommend")) {
+  if (e.bizType == "ad" || e.bizType == "store_recommend") {
     sections.splice(i--, 1);
   }
 }
