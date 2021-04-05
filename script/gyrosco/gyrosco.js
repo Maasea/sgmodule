c780bad9bc1b9b3dbefb49c5d5794a72bea271f3
maasea <351961611@qq.com>
2021-04-05 10:34:29 +0800

    fix

--- script/gyrosco/gyrosco.js
+++ script/gyrosco/gyrosco.js
@@ -1,4 +1,6 @@
 let obj = JSON.parse($response.body);
-obj.user["active_until_time"] = "2099-10-04T00:41:36Z";
-obj.user["x_active_until_time"]="2099-10-04T00:41:36Z"
-$done({body: JSON.stringify(obj)});
+if (obj.user) {
+  obj.user["active_until_time"] = "2099-10-04T00:41:36Z";
+  obj.user["x_active_until_time"] = "2099-10-04T00:41:36Z";
+}
+$done({ body: JSON.stringify(obj) });
