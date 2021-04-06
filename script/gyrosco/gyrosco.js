d78e4e3da7525673eb538d9f67f97169cc751cd0
maasea <351961611@qq.com>
2021-04-06 20:24:38 +0800

    delete useless script

--- script/gyrosco/gyrosco.js
+++
@@ -1,6 +0,0 @@
-let obj = JSON.parse($response.body);
-if (obj.user) {
-  obj.user["active_until_time"] = "2099-10-04T00:41:36Z";
-  obj.user["x_active_until_time"] = "2099-10-04T00:41:36Z";
-}
-$done({ body: JSON.stringify(obj) });
