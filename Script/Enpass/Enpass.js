35bca870b548081080ca7a7d9d2c02e58aa9e870
Maasea <351961611@qq.com>
2021-06-05 16:43:52 +0800

    Uniform format

---
+++ Script/Enpass/Enpass.js
@@ -0,0 +1,5 @@
+var obj = JSON.parse($response.body);
+obj.license="premium"
+obj.info.purchase_type="premium"
+obj.info.store="premium"
+$done({ body: JSON.stringify(obj) });
---
+++ Script/Keep/Sections.js
@@ -0,0 +1,10 @@
+let obj = JSON.parse($response.body);
+let sections = obj.data.sections;
+for (i = 0; i < sections.length; i++) {
+  let e = sections[i];
+  if (e.bizType == "ad" || e.bizType == "store_recommend") {
+    sections.splice(i--, 1);
+  }
+}
+
+$done({ body: JSON.stringify(obj) });
