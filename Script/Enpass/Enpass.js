a79b778e3ff57b3a02b89faf85730f04dfb7ee79
Maasea <351961611@qq.com>
2021-07-16 12:35:31 +0800

    update

--- Script/Enpass/Enpass.js
+++ Script/Enpass/Enpass.js
@@ -1,5 +1,11 @@
 var obj = JSON.parse($response.body);
-obj.license="premium"
-obj.info.purchase_type="premium"
-obj.info.store="premium"
+obj.license = "premium";
+obj.info = {
+  purchase_type: "inapp",
+  store: "ios",
+  id: "ENP_IAP_LTP",
+  userid: "000000",
+  transaction_id: "1000000000000000",
+  logo: "",
+};
 $done({ body: JSON.stringify(obj) });
