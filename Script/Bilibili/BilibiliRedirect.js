d66a56fb88bac9584c4ac19786a9001b578c4667
maasea <351961611@qq.com>
2022-07-05 20:09:27 +0800

    update

--- Script/Bilibili/BilibiliRedirect.js
+++ Script/Bilibili/BilibiliRedirect.js
@@ -1,5 +1,5 @@
 const url = $request.url
-const redirect = url.replace(/https:\/\/d\.bilibili.com\/bi\-li\-bi\-li\-dong\-hua\?.*&schema=/, "")
+const redirect = url.replace(/^https:\/\/d\.bilibili\.com\/bi-li-bi-li-dong-hua\?.*&schema=/, "")
 $done({
     headers: {
         Location: decodeURIComponent(redirect)
