ccc48fd66427649f61a023165f8a3ddc2fefc3da
maasea <351961611@qq.com>
2022-10-20 10:55:26 +0800

    feat: support qx

--- Script/Youtube/youtube-fake.js
+++ Script/Youtube/youtube-fake.js
@@ -1,3 +1,4 @@
-$done({
-    response: { body: "", status: 204 }
-})
+let statusCode = 204
+const isQuanX = typeof $task !== "undefined";
+if (isQuanX) statusCode = "HTTP/1.1 204 No Content"
+$done({ status: statusCode })
