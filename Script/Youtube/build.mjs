f334b1c5cf27f315c335dfbc9a5ee581f6f06464
maasea <351961611@qq.com>
2023-06-07 09:49:45 +0800

    update: (YouTube) complete build.mjs

--- Script/Youtube/build.mjs
+++ Script/Youtube/build.mjs
@@ -14,3 +14,27 @@ esbuild.buildSync({
   target: 'es2020',
   outfile: './dist/youtube.beta.js',
 })
+
+esbuild.buildSync({
+  entryPoints: ['main-response.ts'],
+  bundle: true,
+  minify: !debug,
+  banner: { js: `// Build: ${now}` },
+  inject: ['./lib/text-polyfill.mjs'],
+  sourcemap: false,
+  define: { 'DEBUG': `${debug}` },
+  target: 'es2020',
+  outfile: './dist/youtube.response.beta.js',
+})
+
+esbuild.buildSync({
+  entryPoints: ['main-request.ts'],
+  bundle: true,
+  minify: !debug,
+  banner: { js: `// Build: ${now}` },
+  inject: ['./lib/text-polyfill.mjs'],
+  sourcemap: false,
+  define: { 'DEBUG': `${debug}` },
+  target: 'es2020',
+  outfile: './dist/youtube.request.beta.js',
+})
